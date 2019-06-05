6c63e3124e50b2eec89430f2496d34bcc7d2755e
Hackrhive <hivethehacker@gmail.com>
2019-06-06 01:28:46 +0530

    Login page designed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,73 +12,165 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class homepage extends StatelessWidget{
+
+class homepage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return Login_Signup();
+  }
+
+}
+
+class Login_Signup extends State<homepage>{
+
+  final _formKey = GlobalKey<FormState>();
+  String email,password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Attendance'),
-      ),
+//      appBar: AppBar(
+//        title: Text('Attendance'),
+//      ),
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.blue,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(35, 0, 35, 0),
-                width: double.infinity,
-                child: TextField(
-                  cursorWidth: 8.0,
-                  enableInteractiveSelection: true,
-                  textDirection: TextDirection.ltr,
-                  decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(),
-                    labelText: 'NAME',
-                    labelStyle: TextStyle(
-
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: NetworkImage('https://d3i6fh83elv35t.cloudfront.net/newshour/app/uploads/2015/03/78816800-1024x683.jpg'),
+//            image: NetworkImage('https://www.microrecord.com/wp-content/uploads/2014/04/ecm-for-schools.jpg'),
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.7),  BlendMode.darken)
+          )
+        ),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                      'Attendance Marker',
+                    style: TextStyle(
+                      fontFamily: 'Abril Fatface',
                       fontWeight: FontWeight.bold,
-                      fontSize: 18,
-
+                      color: Colors.white,
+                      fontSize: 32
                     ),
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(35, 20, 35, 0),
-                width: double.infinity,
-                child: TextField(
-                  cursorWidth: 8.0,
-                  enableInteractiveSelection: true,
-                  textDirection: TextDirection.ltr,
-                  decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(),
-                    labelText: 'EMAIL',
-                    labelStyle: TextStyle(
-
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    style: TextStyle(
+                        color: Colors.white
                     ),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hasFloatingPlaceholder: true,
+                      focusedBorder:OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white
+                        ),
+                        borderRadius: BorderRadius.circular(6.0) ,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.white
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(6.0))
+                      ),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value){
+                      if(value.isEmpty){
+                        return 'Empty Input not allowed!';
+                      }
+                    },
+                    onSaved: (value){
+                      setState(() {
+                        email = value;
+                      });
+                    },
                   ),
                 ),
-              ),
-              FlatButton(
-                padding: EdgeInsets.fromLTRB(50, 30, 50, 0),
-//                onPressed: (){Scaffold.of(context).showSnackBar(new SnackBar(content: new Text('Logging in!')));},
-                child: Container(
-
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(5.0)
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hasFloatingPlaceholder: true,
+                      focusedBorder:OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.white
+                        ),
+                        borderRadius: BorderRadius.circular(6.0) ,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))
+                      ),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value){
+                      if(value.isEmpty){
+                        return 'Empty Input not allowed!';
+                      }
+                    },
+                    onSaved: (value){
+                      setState(() {
+                        password = value;
+                      });
+                    },
                   ),
-                  child: Text('SUBMIT DETAILS',style: TextStyle(color: Colors.white),),
                 ),
-              )
-            ],
+                FlatButton(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                    side: BorderSide(color: Colors.white)
+                  ),
+                  textColor: Colors.white,
+                  child: Text('Login'),
+                  onPressed: (){
+                    final form = _formKey.currentState;
+                    if(form.validate()){
+                      form.save();
+                      Alert(
+                          type: AlertType.success,
+                          context: context,
+                          title: "Your info",
+                          desc:
+                          "Email : ${email}\n"
+                              "Password : ${password}",
+                          buttons: [
+                            DialogButton(
+                              child: Text(
+                                "Ok",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20,
+                                    backgroundColor: Color.fromRGBO(7, 156, 255, 100)),
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              width: 50,
+                            )
+                          ]
+                      ).show();
+                    }
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
